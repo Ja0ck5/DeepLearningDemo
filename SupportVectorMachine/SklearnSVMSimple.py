47225836418932636ad96adaf261fb79f08fc91f
ja0ck5 <975117619@qq.com>
2017-11-15 13:42:43 +0800

    comment

--- SupportVectorMachine/SklearnSVMSimple.py
+++ SupportVectorMachine/SklearnSVMSimple.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from sklearn import svm
 
 x = [[2, 0], [1, 1], [2, 3]]
@@ -8,10 +9,10 @@ clf.fit(x, y)
 print clf
 
 # get support vector
-print clf.support_vectors_
+print 'support_vectors_:', clf.support_vectors_
 
-# get indices of support vectors
-print clf.support_
+# get indices of support vectors 支持向量所在的数组的索引位置
+print 'support_:', clf.support_
 
-# get number of support vectors for each class
-print clf.n_support_
+# get number of support vectors for each class 支持向量 分别的个数
+print 'n_support_:', clf.n_support_
